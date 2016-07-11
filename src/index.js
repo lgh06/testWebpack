82f1672bec66c305de84f00b9b437e6ca4eeced9
lgh06 <liugehuan@kankan.com>
2016-07-11 20:56:52 +0800

    babel

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 var lgh = require('./lgh');
+
 lgh();
+
+
+
+ import 'babel-polyfill';
+ import $ from 'jquery';
+
+$(document).ready(function($) {
+  $('<h1>Cats</h1>').appendTo('body');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-    entry: __dirname +'/src/index.js',
-    output: {
-        path: __dirname,
-        filename: 'index.bundle.js'
-    }
+  //entry: ['babel-polyfill', __dirname + '/src/index.js'],
+  entry: [ __dirname + '/src/index.js'],
+  output: {
+    path: __dirname,
+    filename: 'index.bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  }
 };
